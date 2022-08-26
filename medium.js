2f754c9c5236e4e5de34f4f3d2cec4126326b39c
Wadie Mendja <mendja2014@gmail.com>
2022-08-26 22:07:58 +0100

    commit

---
+++ medium.js
@@ -0,0 +1,13 @@
+// URL = https://medium.com/tag/' + medium_topic + '/latest
+function grabArticlesLinks () {
+    const all_articles = document.querySelectorAll(".gy.l");
+    let table = [];
+    all_articles.forEach((element) => {
+        table.push(element.querySelector("a").href);
+    });
+    const linksTable = []; // a table of 10 links (artciles)
+    for (let i = 0; i < table.length; i++)
+        linksTable.push(table[i].substring(0, table[i].indexOf('?')));
+    return  linksTable;
+}
+// URL = 

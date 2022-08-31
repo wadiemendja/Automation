2418217b38056cc5f449fe416ce472e3622b604e
Wadie Mendja <mendja2014@gmail.com>
2022-08-31 18:54:26 +0100

    commit

--- remotask_1.js
+++ remotask_1.js
@@ -1,4 +1,5 @@
 const loop = setInterval(() => {
+    
     const video = document.querySelector('video');
     video.play();
     video.addEventListener("ended", () => {
@@ -8,9 +9,12 @@ const loop = setInterval(() => {
             document.querySelector('.primary').click();
         }, 1000);
     });
+
 }, (document.querySelector('video').duration + 5) * 1000);
 
 setInterval(() => {
+
     if (document.body.innerText.indexOf('Your task queue is empty') == 0)
         clearInterval(loop)
+
 }, 5 * 1000);
